cfd1deda95b912cb9cc8076c90627dd116e90c84
Vu Linh <linhvh.96@gmail.com>
2017-07-19 21:45:13 +0700

    Revert "Develop"

--- app/controllers/static_pages_controller.rb
+++
@@ -1,9 +0,0 @@
-class StaticPagesController < ApplicationController
-  def index; end
-
-  def home; end
-
-  def track; end
-
-  def artist; end
-end
--- app/helpers/application_helper.rb
+++
@@ -1,6 +0,0 @@
-module ApplicationHelper
-  def full_title title = ""
-    base_title = "MusicWorld - The world with music"
-    title.blank? ? base_title : title + " - " + base_title
-  end
-end
